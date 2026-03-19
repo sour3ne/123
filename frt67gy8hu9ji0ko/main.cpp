8b5b701ee1ca169f1971a27ad7a12240fa45cd73
sour3ne <smmatve.y@mail.ru>
2026-03-19 13:26:48 +0500

    Initial Commit

---
+++ frt67gy8hu9ji0ko/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  frt67gy8hu9ji0ko
+//
+//  Created by Матвей Смышляев on 19/03/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
